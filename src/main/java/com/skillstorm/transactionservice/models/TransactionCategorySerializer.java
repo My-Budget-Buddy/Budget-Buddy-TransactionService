c83fe66587fe5554780b578f3db14add68d43d3a
tylercausey <tylercausey1219@gmail.com>
2024-05-21 10:58:14 -0500

    set up serializer and deserializer for category model to make reader-friendly formatting for category names for frontend

---
+++ src/main/java/com/skillstorm/transactionservice/models/TransactionCategorySerializer.java
@@ -0,0 +1,20 @@
+package com.skillstorm.transactionservice.models;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+
+public class TransactionCategorySerializer extends StdSerializer<TransactionCategory> {
+
+    public TransactionCategorySerializer() {
+        super(TransactionCategory.class);
+    }
+
+    @Override
+    public void serialize(TransactionCategory value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeString(value.toString());
+    }
+}
+
